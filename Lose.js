47ae0856c45303d5407e362c8a0089848b9fac1c
taeju.Kwon <44853560+taejuKwon-digipen@users.noreply.github.com>
2020-07-08 19:31:52 +0900

    Add files via upload

---
+++ Lose.js
@@ -0,0 +1,29 @@
+//권태주
+//Prototype
+//CS099
+//Spring 2020
+
+class LoseScene {
+  constructor() {
+    const center_x = width / 2;
+    this.mainmenu = new Button(center_x + 100, height - 50, "Main Menu");
+    this.re = new Button(center_x - 100, height - 50, "restart");
+    this.check = 0;
+  }
+
+  Update() {
+    if (this.mainmenu.DidClickButton()) {
+      CurrentScene = MAIN_MENU;
+    } else if (this.re.DidClickButton()) {
+      play.Reset()
+      CurrentScene = PLAY;
+    }
+    
+  }
+
+  Draw() {
+    DrawTitle("Lose");
+    this.mainmenu.DrawButton();
+    this.re.DrawButton();
+  }
+}
